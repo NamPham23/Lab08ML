d15cd688916343aeb693e8fe1faab47ea12238ad
NamPham23 <152579684+NamPham23@users.noreply.github.com>
2023-12-08 09:06:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pickle as pkl
 import numpy as np
 
 class_list ={'0': 'Negative', '1': 'Neutral','2': 'Positive'}
-st.title('Sentiment analysis from Vietnamese student feedback')
+st.title('Sentiment analysis from Vietnamese names')
 
 #image = Image.open('.png')
 #st.image(image)
